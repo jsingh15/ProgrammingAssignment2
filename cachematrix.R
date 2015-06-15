3107923bf42e4f4f97ca3dbb21419db92c88d294
jsingh <jasdeeps@gmail.com>
2015-06-15 21:37:02 +0530

    prog assignmnet2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+##function makeCacheMatrix stores a matrix and its inverse
+## also stores function to get original matrix, get inverse, populate inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+  xinv <- NULL
+  set <- function(y) {
+    x <<- y
+    xinv <<- NULL
+  }
+  get <- function() x
+  getinverse <- function() xinv
+  ## rather than using setinverse, use populateinverse
+  ## populateinverse ensures that it is not possible to set the value of inverse directly
+  populateinverse <- function() xinv <<- solve(x)
+  list(set = set, get = get,
+       getinverse = getinverse,populateinverse = populateinverse)
 }
-
-
-## Write a short comment describing this function
-
+ 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  xinv <- x$getinverse()
+  ## check if inverse is already stored
+  if(!is.null(xinv)) {
+    message("getting cached data")
+    return(xinv)
+  }
+  ## compute the inverse and store it
+  x$populateinverse()
+  message("computed inverse")
+  ## get the inverse (we know it is there)
+  xinv <- x$getinverse()
+  xinv
 }
+ 
